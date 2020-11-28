b5430494ee493c71713d10586fd21bdf0cc4aa46
LAPTOP-5GU35N3B\Arunkumar <arunkumarchinthapalli0110@gmail.com>
2020-11-28 15:46:52 +0530

    First Commit

---
+++ src/main/java/com/sample/accountService/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.sample.accountService.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "Account Service is Up!";
+    }
+}
