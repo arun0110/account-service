028170ca9616f40764d1d88004836a13d1b33972
LAPTOP-5GU35N3B\Arunkumar <arunkumarchinthapalli0110@gmail.com>
2021-06-15 07:53:12 +0530

    Default Spring Security Added

--- src/main/java/com/sample/accountService/AccountServiceApplication.java
+++ src/main/java/com/sample/accountService/AccountServiceApplication.java
@@ -2,6 +2,9 @@ package com.sample.accountService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AccountServiceApplication {
@@ -9,6 +12,11 @@ public class AccountServiceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
